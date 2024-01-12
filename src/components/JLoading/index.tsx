18d0a8a660f26d52651c5250907c2173d35c4314
Async <1827951482@qq.com>
2024-01-12 22:59:42 +0800

    style: 更换 loading 颜色

--- src/components/JLoading/index.tsx
+++ src/components/JLoading/index.tsx
@@ -4,8 +4,7 @@ const Loading = () => {
 	const style = {
 		width: "100vw",
 		height: "100vh",
-		backgroundColor: "#4b6cb7",
-		background: "linear-gradient(to right, #182848, #1a2848)",
+		backgroundColor: "#18181C",
 		color: "#1db3ff",
 		display: "flex",
 		justifyContent: "center",
