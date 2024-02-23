38fde402cf8ad4e91cc4961150e5301cebe9058c
Async <1827951482@qq.com>
2024-02-23 23:54:44 +0800

    refactor: 重构图表尺寸信息计算，使用 store 中的状态绑定，实现拖拽效果信息实时更新

--- src/components/JChartConfiguration/JSizeSetting/index.tsx
+++ src/components/JChartConfiguration/JSizeSetting/index.tsx
@@ -1,66 +1,31 @@
-import { useEffect, useState } from "react";
 import { InputNumber, Typography } from "antd";
 import JSettingBox from "../public/JSettingBox";
-
 import useEditCharts from "@/hooks/useEditCharts";
-import { ComponentType } from "@/materials/types";
 import useChartStore from "@/store/chartStore/chartStore";
-import { produce } from "immer";
 
 const JSizeSetting = () => {
 	const { updateChartConfig } = useChartStore();
 	const { getTargetChartIndex, getTargetData } = useEditCharts();
 	const chartIndex = getTargetChartIndex()!;
 	const component = getTargetData()!;
-	const [globalConfig, setGlobalGonfig] = useState<{
-		attr: Pick<ComponentType, "attr">["attr"];
-		updateKey: keyof ComponentType["attr"] | "";
-	}>({
-		attr: component!.attr,
-		updateKey: ""
-	});
 
-	useEffect(() => {
-		globalConfig.updateKey &&
-			updateChartConfig(chartIndex, "attr", globalConfig.updateKey, globalConfig["attr"][globalConfig.updateKey]);
-	}, [globalConfig]);
-
-	useEffect(() => {
-		component &&
-			setGlobalGonfig({
-				attr: component.attr,
-				updateKey: ""
-			});
-	}, [chartIndex]);
 	return (
 		<JSettingBox name="尺寸">
 			<div className="w-full flex gap-2">
 				<InputNumber
 					addonBefore={<Typography.Text>宽度</Typography.Text>}
 					className="flex-1"
-					value={globalConfig.attr.w}
+					value={component.attr.w}
 					onChange={(val) => {
-						val &&
-							setGlobalGonfig(
-								produce((draft) => {
-									draft.attr.w = val;
-									draft.updateKey = "w";
-								})
-							);
+						val && updateChartConfig(chartIndex, "attr", null, { ...component.attr, w: val });
 					}}
 				/>
 				<InputNumber
 					addonBefore={<Typography.Text>高度</Typography.Text>}
 					className="flex-1"
-					value={globalConfig.attr.h}
+					value={component.attr.h}
 					onChange={(val) => {
-						val &&
-							setGlobalGonfig(
-								produce((draft) => {
-									draft.attr.h = val;
-									draft.updateKey = "h";
-								})
-							);
+						val && updateChartConfig(chartIndex, "attr", null, { ...component.attr, h: val });
 					}}
 				/>
 			</div>
