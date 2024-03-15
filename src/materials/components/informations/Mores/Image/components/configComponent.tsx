c36f59556396695f152bc12aaa626aea767c7f4e
Async <1827951482@qq.com>
2024-03-16 00:09:03 +0800

    feat: 实现图片组件动态配置

--- src/materials/components/informations/Mores/Image/components/configComponent.tsx
+++ src/materials/components/informations/Mores/Image/components/configComponent.tsx
@@ -1,5 +1,73 @@
+import JCollapseBox from "@/components/JChartConfiguration/public/JCollapseBox";
+import JSettingBox from "@/components/JChartConfiguration/public/JSettingBox";
+import JSettingItem from "@/components/JChartConfiguration/public/JSettingItem";
+import useEditCharts from "@/hooks/useEditCharts";
+import useChartStore from "@/store/chartStore/chartStore";
+import { Input, InputNumber, Select } from "antd";
+
+// 适应类型
+const fitList = [
+	{
+		value: "fill",
+		label: "fill"
+	},
+	{
+		value: "contain",
+		label: "contain"
+	},
+	{
+		value: "cover",
+		label: "cover"
+	},
+	{
+		value: "none",
+		label: "none"
+	}
+];
+
 const ImageConfigComponent = () => {
-	return <></>;
+	const { updateChartConfig } = useChartStore();
+	const { getTargetChartIndex, getTargetData } = useEditCharts();
+	const chartIndex = getTargetChartIndex()!;
+	const component = getTargetData()!;
+	return (
+		<JCollapseBox name="属性" unfold>
+			<>
+				<JSettingBox name="路径">
+					<Input
+						value={component.option.dataset}
+						onChange={(e) => {
+							updateChartConfig(chartIndex, "option", "dataset", e.target.value);
+						}}
+					/>
+				</JSettingBox>
+				<JSettingBox name="样式">
+					<div className="grid grid-cols-2 gap-2">
+						<JSettingItem text="类型">
+							<Select
+								className="w-full"
+								options={fitList}
+								value={component.option.fit}
+								onChange={(val) => {
+									updateChartConfig(chartIndex, "option", "fit", val);
+								}}
+							/>
+						</JSettingItem>
+						<JSettingItem text="圆角">
+							<InputNumber
+								className="w-full"
+								min={0}
+								value={component.option.borderRadius}
+								onChange={(val) => {
+									updateChartConfig(chartIndex, "option", "borderRadius", val);
+								}}
+							/>
+						</JSettingItem>
+					</div>
+				</JSettingBox>
+			</>
+		</JCollapseBox>
+	);
 };
 
 export default ImageConfigComponent;
