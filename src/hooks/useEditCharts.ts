7426fc5b0f159991e1a3184bf147ae4996804a48
Async <1827951482@qq.com>
2024-06-28 22:20:08 +0800

    fix: 修复图表配置视图无法修改问题（没有触发视图更新）

--- src/hooks/useEditCharts.ts
+++ src/hooks/useEditCharts.ts
@@ -2,7 +2,9 @@ import useChartStore from "@/store/chartStore/chartStore";
 import useStoreSelector from "./useStoreSelector";
 
 const useEditCharts = () => {
-	const { getComponentList, getSelectId } = useChartStore(useStoreSelector(["getComponentList", "getSelectId"]));
+	const { getComponentList, componentList, getSelectId } = useChartStore(
+		useStoreSelector(["getComponentList", "getSelectId", "componentList"])
+	);
 	// 获取当前选择的图表在数组中的位置
 	const getTargetChartIndex = (id?: string) => {
 		const selectId = getSelectId();
@@ -18,7 +20,7 @@ const useEditCharts = () => {
 	// 获取当前选择的图表对象
 	const getTargetData = (index?: number) => {
 		const targetIndex = typeof index === "number" ? index : getTargetChartIndex();
-		if (targetIndex !== undefined && targetIndex !== -1) return getComponentList()[targetIndex];
+		if (targetIndex !== undefined && targetIndex !== -1) return componentList[targetIndex];
 	};
 
 	return {
